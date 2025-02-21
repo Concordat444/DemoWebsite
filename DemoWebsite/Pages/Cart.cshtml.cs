dd3a214b7053e99dc9af7c3d23a7558d41344d32
James <jsreed92@gmail.com>
2025-02-21 08:04:58 -0500

    Adds cart icon and switches cart to persist using services

--- DemoWebsite/Pages/Cart.cshtml.cs
+++ DemoWebsite/Pages/Cart.cshtml.cs
@@ -8,19 +8,18 @@ namespace DemoWebsite.Pages
     public class CartModel : PageModel
     {
         private IStoreRepository repository;
-
-        public CartModel(IStoreRepository repo)
+        public CartModel(IStoreRepository repo, Cart cartService)
         {
             repository = repo;
+            Cart = cartService;
         }
 
-        public Cart? Cart { get; set; }
+        public Cart Cart { get; set; }
         public string ReturnUrl { get; set; } = "/";
 
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(long productId, string returnUrl)
@@ -29,10 +28,14 @@ namespace DemoWebsite.Pages
                 .FirstOrDefault(p => p.ProductID == productId);
             if(product != null)
             {
-                Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
                 Cart.AddItem(product, 1);
-                HttpContext.Session.SetJson("cart", Cart);
             }
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+
+        public IActionResult OnPostRemove(long ProductId, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductID == ProductId).Product);
             return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
