bb8519e338261280daa8879410f7dd7a51c95d5c
James <jsreed92@gmail.com>
2025-02-27 06:15:55 -0500

    partially implements admin blazor page

--- DemoWebsite/Program.cs
+++ DemoWebsite/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
 builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
@@ -32,6 +33,8 @@ app.MapControllerRoute("pagination", "Products/Page{productPage}", new { Control
 
 app.MapDefaultControllerRoute();
 app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
 
 SeedStoreData.EnsurePopulated(app);
 
