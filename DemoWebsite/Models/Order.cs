82fd47d2bb6c73741dce4d7b45bf47401f954479
James <jsreed92@gmail.com>
2025-03-03 07:40:33 -0500

    Adds Order tracking and "shipping" features to admin page

--- DemoWebsite/Models/Order.cs
+++ DemoWebsite/Models/Order.cs
@@ -29,5 +29,8 @@ namespace DemoWebsite.Models
         public string? Country { get; set; }
 
         public bool GiftWrap { get; set; }
+
+        [BindNever]
+        public bool Shipped { get; set; }
     }
 }
